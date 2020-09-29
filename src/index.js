d2536b8eb55ba71902d6e21ad950bc09578d4bb0
Maxim Komarov <maksim.komarov01@gmail.com>
2020-09-29 12:34:19 +0300

    feat: add code to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length === 0)
+  {
+    return 0
+  }
+  else {
+    let result = array[0]
+    for (i = 0; i < array.length; i++) {
+      if(result > array[i]){
+          result = array[i]
+      }
+  }
+    return result
+  }  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length === 0)
+  {
+    return 0
+  }
+  else {
+  let result = array[0]
+  for (i = 0; i < array.length; i++) {
+    if(result < array[i]){
+        result = array[i]
+      }
+    }
+    return result
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length === 0)
+  {
+    return 0
+  }
+  else {
+  let result = 0
+  for (i = 0; i < array.length; i++) {
+        result += array[i]
+      }
+    return result / array.length
+    }
 }
